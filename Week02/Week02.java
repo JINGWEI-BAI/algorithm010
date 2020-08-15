d5dd1a2400147743065962e413cd08d103bd9299
JINGWEI-BAI <jingwei.bai@qq.com>
2020-08-16 01:16:45 +0800

    Add files via upload

---
+++ Week02/Week02.java
@@ -0,0 +1,120 @@
+//1.有效的字母异位词，使用哈希表
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length())
+            return false;
+        int[] alpha = new int[26];
+        for(int i = 0; i< s.length(); i++) {
+            alpha[s.charAt(i) - 'a'] ++;
+            alpha[t.charAt(i) - 'a'] --;
+        }
+        for(int i=0;i<26;i++)
+            if(alpha[i] != 0)
+                return false;
+        return true;
+    }
+}
+
+//2.N叉树的前序遍历
+class Solution {
+    public List<Integer> preorder(Node root) {
+        LinkedList<Node> stack = new LinkedList<>();
+        LinkedList<Integer> output = new LinkedList<>();
+        if (root == null) {
+            return output;
+        }
+
+        stack.add(root);
+        while (!stack.isEmpty()) {
+            Node node = stack.pollLast();
+            output.add(node.val);
+            Collections.reverse(node.children);
+            for (Node item : node.children) {
+                stack.add(item);
+            }
+        }
+        return output;
+    }
+}
+
+//3.字母异位词分组
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0) return new ArrayList();
+        Map<String, List> ans = new HashMap<String, List>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+            if (!ans.containsKey(key)) ans.put(key, new ArrayList());
+            ans.get(key).add(s);
+        }
+        return new ArrayList(ans.values());
+    }
+}
+
+//4.丑数
+class Solution {
+    public int nthUglyNumber(int n) {
+        int a = 0, b = 0, c = 0;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        for(int i = 1; i < n; i++) {
+            int n2 = dp[a] * 2, n3 = dp[b] * 3, n5 = dp[c] * 5;
+            dp[i] = Math.min(Math.min(n2, n3), n5);
+            if(dp[i] == n2) a++;
+            if(dp[i] == n3) b++;
+            if(dp[i] == n5) c++;
+        }
+        return dp[n - 1];
+    }
+}
+
+//5.二叉树的中序遍历，使用经典递归。
+class Solution {
+    public List < Integer > inorderTraversal(TreeNode root) {
+        List < Integer > res = new ArrayList < > ();
+        helper(root, res);
+        return res;
+    }
+
+    public void helper(TreeNode root, List < Integer > res) {
+        if (root != null) {
+            if (root.left != null) {
+                helper(root.left, res);
+            }
+            res.add(root.val);
+            if (root.right != null) {
+                helper(root.right, res);
+            }
+        }
+    }
+}
+//6.二叉树的前序遍历
+class Solution {
+  public List<Integer> preorderTraversal(TreeNode root) {
+    LinkedList<TreeNode> stack = new LinkedList<>();
+    LinkedList<Integer> output = new LinkedList<>();
+    if (root == null) {
+      return output;
+    }
+
+    stack.add(root);
+    while (!stack.isEmpty()) {
+      TreeNode node = stack.pollLast();
+      output.add(node.val);
+      if (node.right != null) {
+        stack.add(node.right);
+      }
+      if (node.left != null) {
+        stack.add(node.left);
+      }
+    }
+    return output;
+  }
+}
+
+
+
