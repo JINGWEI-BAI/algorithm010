69e045109dfc4012a615f7b985b948c26ab95797
JINGWEI-BAI <jingwei.bai@qq.com>
2020-08-16 01:32:13 +0800

    Add files via upload

---
+++ Week07/Week07.java
@@ -0,0 +1,240 @@
+//1.爬楼梯，使用DP
+class Solution {
+    public int climbStairs(int n) {
+        int p = 0, q = 0, r = 1;
+        for (int i = 1; i <= n; ++i) {
+            p = q; 
+            q = r; 
+            r = p + q;
+        }
+        return r;
+    }
+}
+
+//2.实现Trie
+public class Trie {
+    private boolean is_string=false;
+    private Trie next[]=new Trie[26];
+
+    public Trie(){}
+
+    public void insert(String word){//插入单词
+        Trie root=this;
+        char w[]=word.toCharArray();
+        for(int i=0;i<w.length;++i){
+            if(root.next[w[i]-'a']==null)root.next[w[i]-'a']=new Trie();
+            root=root.next[w[i]-'a'];
+        }
+        root.is_string=true;
+    }
+
+    public boolean search(String word){//查找单词
+        Trie root=this;
+        char w[]=word.toCharArray();
+        for(int i=0;i<w.length;++i){
+            if(root.next[w[i]-'a']==null)return false;
+            root=root.next[w[i]-'a'];
+        }
+        return root.is_string;
+    }
+    
+    public boolean startsWith(String prefix){//查找前缀
+        Trie root=this;
+        char p[]=prefix.toCharArray();
+        for(int i=0;i<p.length;++i){
+            if(root.next[p[i]-'a']==null)return false;
+            root=root.next[p[i]-'a'];
+        }
+        return true;
+    }
+}
+
+//3.朋友圈，使用并查集
+public class Solution {
+    int find(int parent[], int i) {
+        if (parent[i] == -1)
+            return i;
+        return find(parent, parent[i]);
+    }
+
+    void union(int parent[], int x, int y) {
+        int xset = find(parent, x);
+        int yset = find(parent, y);
+        if (xset != yset)
+            parent[xset] = yset;
+    }
+    public int findCircleNum(int[][] M) {
+        int[] parent = new int[M.length];
+        Arrays.fill(parent, -1);
+        for (int i = 0; i < M.length; i++) {
+            for (int j = 0; j < M.length; j++) {
+                if (M[i][j] == 1 && i != j) {
+                    union(parent, i, j);
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < parent.length; i++) {
+            if (parent[i] == -1)
+                count++;
+        }
+        return count;
+    }
+}
+
+//4.岛屿数量，使用并查集
+class Solution {
+    class UnionFind {
+        int count;
+        int[] parent;
+        int[] rank;
+
+        public UnionFind(char[][] grid) {
+            count = 0;
+            int m = grid.length;
+            int n = grid[0].length;
+            parent = new int[m * n];
+            rank = new int[m * n];
+            for (int i = 0; i < m; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    if (grid[i][j] == '1') {
+                        parent[i * n + j] = i * n + j;
+                        ++count;
+                    }
+                    rank[i * n + j] = 0;
+                }
+            }
+        }
+
+        public int find(int i) {
+            if (parent[i] != i) parent[i] = find(parent[i]);
+            return parent[i];
+        }
+
+        public void union(int x, int y) {
+            int rootx = find(x);
+            int rooty = find(y);
+            if (rootx != rooty) {
+                if (rank[rootx] > rank[rooty]) {
+                    parent[rooty] = rootx;
+                } else if (rank[rootx] < rank[rooty]) {
+                    parent[rootx] = rooty;
+                } else {
+                    parent[rooty] = rootx;
+                    rank[rootx] += 1;
+                }
+                --count;
+            }
+        }
+
+        public int getCount() {
+            return count;
+        }
+    }
+
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+        UnionFind uf = new UnionFind(grid);
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    grid[r][c] = '0';
+                    if (r - 1 >= 0 && grid[r-1][c] == '1') {
+                        uf.union(r * nc + c, (r-1) * nc + c);
+                    }
+                    if (r + 1 < nr && grid[r+1][c] == '1') {
+                        uf.union(r * nc + c, (r+1) * nc + c);
+                    }
+                    if (c - 1 >= 0 && grid[r][c-1] == '1') {
+                        uf.union(r * nc + c, r * nc + c - 1);
+                    }
+                    if (c + 1 < nc && grid[r][c+1] == '1') {
+                        uf.union(r * nc + c, r * nc + c + 1);
+                    }
+                }
+            }
+        }
+
+        return uf.getCount();
+    }
+}
+//5.N皇后，使用回溯
+
+class Solution {
+  int rows[];
+  // "hill" diagonals
+  int hills[];
+  // "dale" diagonals
+  int dales[];
+  int n;
+  // output
+  List<List<String>> output = new ArrayList();
+  // queens positions
+  int queens[];
+
+  public boolean isNotUnderAttack(int row, int col) {
+    int res = rows[col] + hills[row - col + 2 * n] + dales[row + col];
+    return (res == 0) ? true : false;
+  }
+
+  public void placeQueen(int row, int col) {
+    queens[row] = col;
+    rows[col] = 1;
+    hills[row - col + 2 * n] = 1;  // "hill" diagonals
+    dales[row + col] = 1;   //"dale" diagonals
+  }
+
+  public void removeQueen(int row, int col) {
+    queens[row] = 0;
+    rows[col] = 0;
+    hills[row - col + 2 * n] = 0;
+    dales[row + col] = 0;
+  }
+
+  public void addSolution() {
+    List<String> solution = new ArrayList<String>();
+    for (int i = 0; i < n; ++i) {
+      int col = queens[i];
+      StringBuilder sb = new StringBuilder();
+      for(int j = 0; j < col; ++j) sb.append(".");
+      sb.append("Q");
+      for(int j = 0; j < n - col - 1; ++j) sb.append(".");
+      solution.add(sb.toString());
+    }
+    output.add(solution);
+  }
+
+  public void backtrack(int row) {
+    for (int col = 0; col < n; col++) {
+      if (isNotUnderAttack(row, col)) {
+        placeQueen(row, col);
+        // if n queens are already placed
+        if (row + 1 == n) addSolution();
+          // if not proceed to place the rest
+        else backtrack(row + 1);
+        // backtrack
+        removeQueen(row, col);
+      }
+    }
+  }
+
+  public List<List<String>> solveNQueens(int n) {
+    this.n = n;
+    rows = new int[n];
+    hills = new int[4 * n - 1];
+    dales = new int[2 * n - 1];
+    queens = new int[n];
+
+    backtrack(0);
+    return output;
+  }
+}
+
+
+
