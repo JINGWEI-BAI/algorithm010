b345a0d1434c297b69062b007a04e0ed453428d5
JINGWEI-BAI <jingwei.bai@qq.com>
2020-08-16 13:44:56 +0800

    Add files via upload

---
+++ Week06/Week06.java
@@ -0,0 +1,204 @@
+//1.最小路径和，使用DP
+class Solution {
+    public int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int rows = grid.length, columns = grid[0].length;
+        int[][] dp = new int[rows][columns];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < rows; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int j = 1; j < columns; j++) {
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+        }
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < columns; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[rows - 1][columns - 1];
+    }
+}
+
+//2.回文子串
+class Solution {
+    public int countSubstrings(String s) {
+        if(s == null || s.equals("")){
+            return 0;
+        }
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int result = s.length();
+        for(int i = 0; i<n; i++) dp[i][i] = true;
+        for(int i = n-1; i>=0; i--){
+            for(int j = i+1; j<n; j++){
+                if(s.charAt(i) == s.charAt(j)) {
+                    if(j-i == 1){
+                        dp[i][j] = true;
+                    }
+                    else{
+                        dp[i][j] = dp[i+1][j-1]; 
+                    }
+                }else{
+                    dp[i][j] = false;
+                }
+                if(dp[i][j]){
+                    result++;
+                }
+            }
+        }
+        return result;
+        
+    }
+}
+
+//3.解码方法
+public class Solution {
+
+    public int numDecodings(String s) {
+        int len = s.length();
+        if (len == 0) {
+            return 0;
+        }
+
+        // dp[i] 以 s[i] 结尾的前缀子串有多少种解码方法
+        // dp[i] = dp[i - 1] * 1 if s[i] != '0'
+        // dp[i] += dp[i - 2] * 1 if  10 <= int(s[i - 1..i]) <= 26
+        int[] dp = new int[len];
+
+        char[] charArray = s.toCharArray();
+        if (charArray[0] == '0') {
+            return 0;
+        }
+        dp[0] = 1;
+
+        for (int i = 1; i < len; i++) {
+            if (charArray[i] != '0') {
+                dp[i] = dp[i - 1];
+            }
+
+            int num = 10 * (charArray[i - 1] - '0') + (charArray[i] - '0');
+            if (num >= 10 && num <= 26) {
+                if (i == 1) {
+                    dp[i]++;
+                } else {
+                    dp[i] += dp[i - 2];
+                }
+            }
+        }
+        return dp[len - 1];
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "10";
+        int res = solution.numDecodings(s);
+        System.out.println(res);
+    }
+}
+
+//4.最大正方形
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int maxSide = 0;
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return maxSide;
+        }
+        int rows = matrix.length, columns = matrix[0].length;
+        int[][] dp = new int[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (matrix[i][j] == '1') {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    }
+                    maxSide = Math.max(maxSide, dp[i][j]);
+                }
+            }
+        }
+        int maxSquare = maxSide * maxSide;
+        return maxSquare;
+    }
+}
+
+//5.青蛙过河，使用DP
+public class Solution {
+    public boolean canCross(int[] stones) {
+        HashMap<Integer, Set<Integer>> map = new HashMap<>();
+        for (int i = 0; i < stones.length; i++) {
+            map.put(stones[i], new HashSet<Integer>());
+        }
+        map.get(0).add(0);
+        for (int i = 0; i < stones.length; i++) {
+            for (int k : map.get(stones[i])) {
+                for (int step = k - 1; step <= k + 1; step++) {
+                    if (step > 0 && map.containsKey(stones[i] + step)) {
+                        map.get(stones[i] + step).add(step);
+                    }
+                }
+            }
+        }
+        return map.get(stones[stones.length - 1]).size() > 0;
+    }
+}
+
+//6.最长有效括号，使用DP
+public class Solution {
+    public int longestValidParentheses(String s) {
+        int maxans = 0;
+        int dp[] = new int[s.length()];
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                    dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+                }
+                maxans = Math.max(maxans, dp[i]);
+            }
+        }
+        return maxans;
+    }
+}
+
+//7.编辑距离。DP
+class Solution {
+  public int minDistance(String word1, String word2) {
+    int n = word1.length();
+    int m = word2.length();
+
+    // 有一个字符串为空串
+    if (n * m == 0)
+      return n + m;
+
+    // DP 数组
+    int [][] D = new int[n + 1][m + 1];
+
+    // 边界状态初始化
+    for (int i = 0; i < n + 1; i++) {
+      D[i][0] = i;
+    }
+    for (int j = 0; j < m + 1; j++) {
+      D[0][j] = j;
+    }
+
+    // 计算所有 DP 值
+    for (int i = 1; i < n + 1; i++) {
+      for (int j = 1; j < m + 1; j++) {
+        int left = D[i - 1][j] + 1;
+        int down = D[i][j - 1] + 1;
+        int left_down = D[i - 1][j - 1];
+        if (word1.charAt(i - 1) != word2.charAt(j - 1))
+          left_down += 1;
+        D[i][j] = Math.min(left, Math.min(down, left_down));
+
+      }
+    }
+    return D[n][m];
+  }
+}
+
